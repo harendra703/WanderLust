6e2f8d6ef225ef5e81c62df1c97a909c47395f59
Harendra Kumar <harendrakumar852414@gmail.com>
2026-08-26 18:26:37 +0530

    Fix MongoStore and session configuration

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ if(process.env.NODE_ENV !="production"){
     require('dotenv').config()
 }
 
-// console.log(process.env.SECRET)
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -12,7 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const { MongoStore } = require("connect-mongo");
+const MongoStore = require("connect-mongo").default || require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
